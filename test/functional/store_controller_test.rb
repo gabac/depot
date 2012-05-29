eb24cd819d3be978bfdea83cfa4971fe408d176b
Cyril Gabathuler <gabathuler@gmail.com>
2012-05-29 14:27:39 +0200

    fixed tests

--- test/functional/store_controller_test.rb
+++ test/functional/store_controller_test.rb
@@ -4,8 +4,8 @@ class StoreControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
-    assert_select '#columns #side a', minimum: 4
-    assert_select '#main .entry', 3
+    assert_select '.nav li a', minimum: 4
+    assert_select '.store .entry', 3
     assert_select 'h3', 'Programming Ruby 1.9'
     assert_select '.price', /\$[,\d]+\.\d\d/
   end
